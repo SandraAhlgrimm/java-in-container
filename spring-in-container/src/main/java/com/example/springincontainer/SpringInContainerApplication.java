1e7f2a6a6e42aac54ef9b20431ba0502ad0f93f2
SandraAhlgrimm <sakriema@microsoft.com>
2021-12-10 16:12:51 +0100

    sample benchmark app in spring

---
+++ spring-in-container/src/main/java/com/example/springincontainer/SpringInContainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springincontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInContainerApplication.class, args);
+    }
+
+}
